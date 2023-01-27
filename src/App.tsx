d1b047fb8e362a3313b2954860ba80afdb2c3e09
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2023-01-27 11:03:13 -0500

    Finished Navbar and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Navbar from "./scenes"
 import  {SelectedPage} from "@/shared/types"
+import Footer from "@/scenes/footer"
+
 
 function App() {
 
 const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+const [navTop, setNavTop] = useState<boolean>(true)
 
+useEffect(() => {
+  const handleScroll = () => {
+    if (window.scrollY === 0){
+      setNavTop(true);
+      setSelectedPage(SelectedPage.Home)
+    }
+    if (window.scrollY !== 0) setNavTop(false)
+  }
+  window.addEventListener("Scroll", handleScroll)
+  return () => window.removeEventListener("scroll", handleScroll)
+}, []);
 
 return (
-    <div className="app bg-extra2">
+    <div className="app">
       <Navbar
+      navTop={navTop}
       selectedPage = {selectedPage}
       setSelectedPage = {setSelectedPage} 
       />
+      <Footer />
     </div>
   )
 }
